518968d5b2df19a573776311af76ad96092377d8
Moritz Mueller <moritz@schaefermueller.de>
2018-02-20 18:48:28 +0100

    done the booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,24 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :new]
   def index
-    @stripper = Stripper.find(parmas[:stripper_id]) #how do we involve the user(_id) since we don't have a users-controller?
-    @booking.stripper = @stripper    
+    @stripper = Stripper.find(params[:stripper_id]) #how do we involve the user(_id) since we don't have a users-controller?
+    @booking.stripper = @stripper
     @bookings = Booking.all
   end
 
   def show
     find_booking
-    @stripper = Stripper.find(parmas[:stripper_id])
+    @stripper = Stripper.find(params[:stripper_id])
     @booking.stripper = @stripper
   end
 
   def new
-    @stripper = Stripper.find(parmas[:stripper_id])
+    @stripper = Stripper.find(params[:stripper_id])
     @booking = Booking.new
   end
 
   def create
-    @stripper = Stripper.find(parmas[:stripper_id])
+    @stripper = Stripper.find(params[:stripper_id])
     @booking = Booking.new(booking_params)
     @booking.stripper = @stripper
     @booking.user = current_user

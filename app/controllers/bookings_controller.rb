10882f57a4739dd9edffa6a5805f8ae5ca21c77d
Marcus Zierke <marcuszierke@gmail.com>
2018-02-20 09:13:36 +0100

    seed logo

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,46 @@
 class BookingsController < ApplicationController
+
   def index
-    
+    @stripper = Stripper.find(parmas[:stripper_id]) #how do we involve the user(_id) since we don't have a users-controller?
+    @booking.stripper = @stripper    
+    @bookings = Booking.all
   end
 
   def show
-    
-  end
-
-  def create
-    
+    find_booking
+    @stripper = Stripper.find(parmas[:user_id])
+    @booking.stripper = @stripper
   end
 
   def new
-    
+    @stripper = Stripper.find(parmas[:stripper_id])
+    @booking = Booking.new
   end
 
+  def create
+    @stripper = Stripper.find(parmas[:stripper_id])
+    @booking = Booking.new(booking_params)
+    @booking.stripper = @stripper
+    if @booking.save
+      redirect_to bookings_path(@booking) #since we don't have a users-controller redirect_to the @booking page?
+    end
+  end
+
+
   def destroy
-    
+    find_booking
+    @booking.destroy
+    redirect_to strippers_path
+  end
+
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:bookings).permit(:user_id, :stripper_id, :starts_at, :ends_at)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+c = 1
+
+100.times do
+  name = Faker::Superhero.name
+  description = Faker::Lorem.paragraph(5)
+  price = (55..294).to_a.sample
+  review = (0..50).to_a.sample.to_f / 10
+  city = %w(Berlin Barcelona Hamburg Sydney London Paris Madrid Cologne Frankfurt Rio).sample
+  height = (165..203).to_a.sample
+  hair_color = ['brown', 'black', 'blond', 'white', 'light-brown', 'dark-brown', 'dark-blond', 'ginger'].sample
+  eye_color = %W(green blue green-grey blue-grey hazel brown).sample
+  age = (18..65).to_a.sample
+  Stripper.create(name: name, price: price, review: review, description: description, city: city, height: height, hair_color: hair_color, eye_color: eye_color, age: age)
+  p "worked, number #{c}"
+  c += 1;
+end
+
+# how to handle those parms: :ethnicity, :characters, :solo, :availability, :pics?

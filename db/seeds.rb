fbe12fba15b0050d94d10bf06ec8e7ffebb09f96
Julian Jeczawitz <julzberlin@gmail.com>
2018-02-22 15:38:50 +0100

    updated video position and changed show-booking page

--- db/seeds.rb
+++ db/seeds.rb
@@ -158,40 +158,34 @@ Stripper.create(password: "password", email: "tuan@lewagon.com", name: "The Pene
   eye_color: "brown", age: 48, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519213984/2CAAE4BE-19BD-4D29-92DE-A4890B32D66A.jpg', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519224833/Bildschirmfoto_2018-02-21_um_15.51.23.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519224833/Bildschirmfoto_2018-02-21_um_15.52.19.png', pics3: 'http://res.cloudinary.com/dncveixad/video/upload/v1519305869/Tuanito.mp4')
 
 Stripper.create(password: "password", email: "Vini@lewagon.com", name: "Gray Fox",
-  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "I'm like pi baby, I'm really long and I go on forever.*", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 129, review: 5,
+  description: "I'm like pi baby, I'm really long and I go on forever.*A man of mystery and power, whose power is exceeded only by his mystery, Generally, the path of least resistance appeals. Also, I am excellent at parallel parking.", city: "Bangcock", height: 181, hair_color: "dark-brown",
   eye_color: "brown", age: 18, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293609/Screen_Shot_2018-02-22_at_10.56.40.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_10.57.43.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293607/Screen_Shot_2018-02-22_at_10.58.52.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293238/383.jpg')
-
 Stripper.create(password: "password", email: "anton@lewagon.com", name: "Biggus Diccus",
-  ethnicity: "white", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "On a scale from 1 to 10, you're a 9...... And I'm the 1 you need.*", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  ethnicity: "white", characters: "motz prof cowboy santa cab waiter bowling",price: 65, review: 5,
+  description: "On a scale from 1 to 10, you're a 9...... And I'm the 1 you need.*Fabulous ends in “us” coincidence? I think not", city: "Bangcock", height: 181, hair_color: "dark-brown",
   eye_color: "brown", age: 60, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294475/Screen_Shot_2018-02-22_at_11.13.41.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294475/Screen_Shot_2018-02-22_at_11.13.52.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.13.25.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294474/Screen_Shot_2018-02-22_at_11.13.16.png')
+Stripper.create(password: "password", email: "Konstantinos@lewagon.com", name: "Daddy",
+  ethnicity: "White", characters: "motz prof cowboy santa cab waiter bowling",price: 79, review: 5,
+  description: "You can call me The Fireman....mainly because I turn the hoes on*Oh Hey, you like it dirty too?", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  eye_color: "brown", age: 18, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519308427/konstantinos-mitsainas-foto.1024x1024.jpg', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png')
+Stripper.create(password: "password", email: "Rich@lewagon.com", name: "Helluva Booty Carter",
+  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 56, review: 5,
+  description: "Hey baby take my booty and lets be pirates.*I feel sorry for people who don‘t know me.", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  eye_color: "brown", age: 35, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293980/Screen_Shot_2018-02-22_at_11.01.38.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png')
+Stripper.create(password: "password", email: "Philipp@lewagon.com", name: "Thunder Nuts",
+  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 123, review: 5,
+  description: "Hey, wanna come over to Myspace and Twitter with my Yahoo till I Google all over your Facebook?*Everyone who knows me can be divided into two groups: those who like me and those who still don’t know me.", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  eye_color: "brown", age: 19, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294145/Screen_Shot_2018-02-22_at_11.07.41.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294145/Screen_Shot_2018-02-22_at_11.07.41.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294145/Screen_Shot_2018-02-22_at_11.07.41.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294145/Screen_Shot_2018-02-22_at_11.07.41.png')
+Stripper.create(password: "password", email: "Taka@lewagon.com", name: "Juana Bang",
+  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 969, review: 5,
+  description: "Would you like Gin and Platonic, or do you prefer Scotch and Sofa?*I never thought I had such an exciting life, before I heard what people talk about me.", city: "Bangcock", height: 181, hair_color: "dark-brown",
+  eye_color: "brown", age: 45, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293976/Screen_Shot_2018-02-22_at_11.05.00.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293976/Screen_Shot_2018-02-22_at_11.05.00.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293976/Screen_Shot_2018-02-22_at_11.05.00.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293976/Screen_Shot_2018-02-22_at_11.05.00.png')
 
-Stripper.create(password: "password", email: "Konstantions@lewagon.com", name: "Daddy",
-  ethnicity: "White", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "You can call me The Fireman....mainly because I turn the hoes on*", city: "Bangcock", height: 181, hair_color: "dark-brown",
-  eye_color: "brown", age: 18, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519294717/konstantinos-mitsainas-foto.1024x1024.jpg', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519295924/Screen_Shot_2018-02-22_at_11.01.01.png')
-
-Stripper.create(password: "password", email: "Rich@lewagon.com", name: "",
-  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "Do you believe in love at first sight or shall I walk by again?*", city: "Bangcock", height: 181, hair_color: "dark-brown",
-  eye_color: "brown", age: 18, pics1: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293980/Screen_Shot_2018-02-22_at_11.01.38.png', pics2: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png', pics3: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png', pics4: 'http://res.cloudinary.com/dncveixad/image/upload/v1519293981/Screen_Shot_2018-02-22_at_11.03.00.png')
-
-Stripper.create(password: "password", email: "vini@lewagon.com", name: "",
-  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "Hey, wanna come over to Myspace and Twitter with my Yahoo till I Google all over your Facebook?*", city: "Bangcock", height: 181, hair_color: "dark-brown",
-  eye_color: "brown", age: 18, pics1: '', pics2: '', pics3: '', pics4: '')
-
-Stripper.create(password: "password", email: "vini@lewagon.com", name: "",
-  ethnicity: "asian", characters: "motz prof cowboy santa cab waiter bowling",price: 69, review: 5,
-  description: "Do you believe in love at first sight or shall I walk by again?*", city: "Bangcock", height: 181, hair_color: "dark-brown",
-  eye_color: "brown", age: 18, pics1: '', pics2: '', pics3: '', pics4: '')
 
 User.create(email: "tuan@lewagon.com", password: "password", avatar: p1, name: "Tuan Pererea")
 User.create(email: "julian@lewagon.com", password: "password", avatar: p2, name: "Julian Lovelace")
 User.create(email: "moritz@lewagon.com", password: "password", avatar: p3, name: "Moritz Motz")
 User.create(email: "marcus@lewagon.com", password: "password", avatar: p4, name: "Mickey Mahoney")
-
-
 
 

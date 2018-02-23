91284ddafb058929f84ac90070246f91b272b857
Marcus Zierke <marcuszierke@gmail.com>
2018-02-23 01:24:57 +0100

    updated characters + layout

--- config/initializers/algoliasearch.rb
+++
@@ -1,9 +0,0 @@
-AlgoliaSearch.configuration = {
-  application_id: 'PK1UVHLTSV',
-  api_key: '8718ade0e589d6bac68d1be25fc463bc',
-  connect_timeout: 2,
-  receive_timeout: 30,
-  send_timeout: 30,
-  batch_timeout: 120,
-  search_timeout: 5
-}

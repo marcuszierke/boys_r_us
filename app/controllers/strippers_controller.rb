4fde4329df47b9fe8f89cd6a862cb1163cd72a21
Marcus Zierke <marcuszierke@gmail.com>
2018-05-04 23:05:55 +0200

    responsive stripper index + stripper show

--- app/controllers/strippers_controller.rb
+++ app/controllers/strippers_controller.rb
@@ -1,6 +1,4 @@
 class StrippersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-
   def index
     @strippers = policy_scope(Stripper)
     @strippers = Stripper.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'staticpages/index'
+  get 'strippers/index'
   
   resources :strippers, only: [ :index, :show] do
     resources :bookings, only: [ :new, :create]
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   
   devise_for :strippers
   devise_for :users
-  root to: 'staticpages#index'
+  root to: 'strippers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

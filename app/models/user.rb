af13f34b4d075b8ad1e9bf71327d3f9dc6cc8be3
Marcus Zierke <marcuszierke@gmail.com>
2018-05-11 13:14:43 +0200

    dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :bookings
-  has_many :strippers, through: :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :strippers, through: :bookings, dependent: :destroy
 end
